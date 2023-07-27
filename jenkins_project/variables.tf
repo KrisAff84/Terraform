2be80c3f321fd79280007838df1c45e9572d5864
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-07-26 23:16:46 -0500

    Corrected jenkins_project/main.tf

--- jenkins_project/variables.tf
+++ jenkins_project/variables.tf
@@ -1,10 +1,15 @@
 variable "my_ip" {
-    type    = string
-    default = " "
-    sensitive = true
-} 
+  type      = string
+  default   = " "
+  sensitive = true
+}
 
 variable "ami" {
-    type    = string
-    default = "ami-08541bb85074a743a"
+  type    = string
+  default = "ami-08541bb85074a743a"
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "jenkins-artifacts-0726"
 }
