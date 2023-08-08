4e4607a9d08accadc991eab8e90f79491529784d
Kristoffer Afflerbaugh <128734866+KrisAff84@users.noreply.github.com>
2023-08-08 16:50:12 -0500

    Delete course_section9_variables directory

--- course_section9_variables/main.tf
+++
@@ -1,9 +0,0 @@
-# To create an environment variable
-# export TV_VAR_<variable_name>=<variable_value>
-# export TF_VAR_region=us-east-1 (In command line)
-
-# Most popular way to set variables is through .tfvars file
-
-# Can also assign values to variables during terraform plan/apply
-# terraform plan -var variables_<variable_name>=<variable_value>
-# terraform apply -var variables_<variable_name>=<variable_value>
